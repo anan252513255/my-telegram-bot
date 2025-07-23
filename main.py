0cd5dff9d5ce31cdb6f600b14b8c0520d9c625c9
anan552513255 <anan552513255@gmail.com>
2025-07-23 20:24:20 +0800

    Fix lấy token từ biến môi trường

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("7980211121:AAHq9v27S5YMIowrVQJnhWcZqkF2zwNt_G0")  # Lấy token từ biến môi trường
+TOKEN = os.getenv("BOT_TOKEN")  # Lấy token từ biến môi trường
+
+if not TOKEN:
+    raise ValueError("❌ BOT_TOKEN không tồn tại. Kiểm tra biến môi trường trên Render.")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Bot đã chạy trên Render.")
